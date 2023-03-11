0873913f3f1920d030319a44bf3ff5096dc06678
Iheb Gammar <iheb.gammar@yahoo.fr>
2023-03-11 22:41:44 +0100

    add 3 files

---
+++ Frequency_and_wordcloud.py
@@ -0,0 +1,81 @@
+import csv
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# Download the stopwords corpus
+import nltk
+nltk.download('stopwords')
+
+# Open the CSV file for reading
+with open('dt.csv', 'r', encoding='utf-8') as input_file:
+
+    # Create a CSV reader object
+    reader = csv.reader(input_file, delimiter=',')
+
+    # Create an empty list to hold the words
+    words = []
+
+    # Define stopwords to remove
+    stop_words = set(stopwords.words('english'))
+
+    # Iterate through each row of the CSV file
+    for row in reader:
+
+        # Remove links and punctuation marks
+        text = re.sub(r'http\S+', '', row[0])
+        text = re.sub(r'[^\w\s]', '', text)
+
+        # Tokenize the text using word_tokenize
+        tokens = word_tokenize(text)
+
+        # Remove stopwords and demonstrative words
+        filtered_tokens = [token.lower() for token in tokens if token.lower() not in stop_words and token.lower() not in ['this', 'that', 'these', 'those']]
+
+        # Add the filtered tokens to the words list
+        words.extend(filtered_tokens)
+
+# Create a frequency distribution of the words
+freq_dist = FreqDist(words)
+
+# Get the 25 most frequent words and least frequent words
+most_frequent_words = freq_dist.most_common(25)
+least_frequent_words = freq_dist.most_common()[-25:]
+
+# Create a bar graph for the most frequent words
+plt.figure(figsize=(10,5))
+plt.bar([word[0] for word in most_frequent_words], [word[1] for word in most_frequent_words])
+plt.xticks(rotation=90)
+plt.title("Most frequent words")
+plt.xlabel("Words")
+plt.ylabel("Frequency")
+plt.tight_layout()
+plt.savefig("bar_graph_most_frequent.png")
+
+# Create a bar graph for the least frequent words
+plt.figure(figsize=(10,5))
+plt.bar([word[0] for word in least_frequent_words], [word[1] for word in least_frequent_words])
+plt.xticks(rotation=90)
+plt.title("Least frequent words")
+plt.xlabel("Words")
+plt.ylabel("Frequency")
+plt.tight_layout()
+plt.savefig("bar_graph_least_frequent.png")
+
+# Generate a word cloud
+wordcloud = WordCloud(width=800, height=800, background_color='white').generate_from_frequencies(freq_dist)
+
+# Plot the word cloud
+plt.figure(figsize=(8, 8), facecolor=None)
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.tight_layout(pad=0)
+
+# Save the word cloud as a PNG file
+plt.savefig("wordcloud.png")
+
+# Show the plots
+plt.show()
---
+++ import_csv.py
@@ -0,0 +1,77 @@
+import csv
+import re
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# Download stopwords if not already downloaded
+import nltk
+nltk.download('stopwords')
+
+# Open the CSV file for reading
+with open('idss.csv', 'r', encoding='utf-8') as ids_file:
+    # Create a CSV reader object
+    ids_reader = csv.reader(ids_file)
+    # Create three empty lists to hold the IDs for each subset
+    healthy_ids = []
+    neutral_ids = []
+    unhealthy_ids = []
+    # Iterate through each row of the CSV file
+    for row in ids_reader:
+        # Check the label in the second column and add the ID to the appropriate list
+        if row[0] == 'healthy':
+            healthy_ids.append(row[0])
+        elif row[0] == 'neutral':
+            neutral_ids.append(row[0])
+        elif row[0] == 'unhealthy':
+            unhealthy_ids.append(row[0])
+
+# Define a function to preprocess the text by removing punctuation, stop words, links, and demonstrative words
+def preprocess_text(text):
+    # Remove links
+    text = re.sub(r'http\S+', '', text)
+    # Tokenize the text using NLTK's word_tokenize function
+    tokens = word_tokenize(text)
+    # Remove punctuation marks and lowercase the words
+    words = [word.lower() for word in tokens if word.isalpha()]
+    # Remove stop words
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+    # Remove demonstrative words
+    words = [word for word in words if word not in ['this', 'that', 'these', 'those']]
+    return words
+
+# Define a function to generate a word cloud and a bar chart for a given subset
+def generate_visualizations(subset_name, ids):
+    # Open the CSV file for reading
+    with open('dt.csv', 'r', encoding='utf-8') as input_file:
+        # Create a CSV reader object
+        reader = csv.reader(input_file)
+        # Create an empty list to hold the words
+        words = []
+        # Iterate through each row of the CSV file
+        for row in reader:
+            # Check if the ID is in the current subset
+            if row[1] in ids:
+                # Preprocess the text
+                tokens = preprocess_text(row[0])
+                print(row)
+                # Add the tokens to the words list
+                words.extend(tokens)
+    # Create a frequency distribution of the words
+    freq_dist = FreqDist(words)
+    # Get the 25 most frequent words and least frequent words
+    most_frequent_words = freq_dist.most_common(25)
+    least_frequent_words = freq_dist.most_common()[-25:]
+    # Print the most frequent and least frequent words
+    print(f"Most frequent words in {subset_name}:")
+    for word in most_frequent_words:
+        print(word[0], "-", word[1])
+    print(f"\nLeast frequent words in {subset_name}:")
+    for word in least_frequent_words:
+        print(word[0], "-", word[1])
+    # Generate a word cloud
+    print('aaa')
+    wordcloud = WordCloud(width=800)
